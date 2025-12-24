0e28900391b9712d5dce0cca650e598605f797df
SippinMyCoffee <memoyacinhmaid@gmail.com>
2025-12-25 00:35:54 +0200

    Create omni_obsidian.mk

---
+++ omni_obsidian.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_DEVICE := obsidian
+PRODUCT_NAME := omni_obsidian
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 14 Pro 4G
+PRODUCT_MANUFACTURER := xiaomi
